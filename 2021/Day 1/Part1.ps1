82bb678576a537b4f209f66aa332866e7b61202b
Jeremy Putman <putman.jeremy@gmail.com>
2021-12-03 02:11:58 -0500

    Removed Some Cluttered WriteHost

--- 2021/Day 1/Part1.ps1
+++ 2021/Day 1/Part1.ps1
@@ -1,14 +1,9 @@
 Clear-Host
 $data = Get-Content '.\Data.txt'
 $larger = 0;
-Write-Host $data[0] '(N/A - no previous measurement)'
 for($count = 1; $count -lt $data.Count; $count++){
   if([int]$data[$count] -gt [int]$data[$count-1]){
-    Write-Host $data[$count] '(increased)'
     $larger++
   }
-  else{
-    Write-Host $data[$count] '(decreased)'
-  }
 }
-Write-Host 'There are ' $larger ' measurements that are larger than the previous measurement.'
+Write-Host 'There are ' $larger ' sums that are larger than the previous sum.'
--- 2021/Day 1/Part2.ps1
+++ 2021/Day 1/Part2.ps1
@@ -4,18 +4,10 @@ $larger = 0;
 for($count = 0; $count -lt $data.count -2; $count++){
   $sum = [int]$data[$count] + [int]$data[$count + 1] + [int]$data[$count + 2]
   if($count -eq 0){
-    Write-Host $sum '(N/A - no previous sum)'
   }
   else{
     if($sum -gt $previous){
       $larger++
-      Write-Host $sum '(increased)'
-    }
-    elseif($sum -eq $previous){
-      Write-Host $sum '(no change)'
-    }
-    else{
-      Write-Host $sum '(decreased)'
     }
   }
   $previous = $sum
