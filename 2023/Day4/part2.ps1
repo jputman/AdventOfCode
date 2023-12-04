07ad586c74b6d2cdd4c97678aa817a991bf5bf20
Jeremy Putman <jeremy.putman@ugdsb.on.ca>
2023-12-04 16:30:27 -0500

    day 4 done

--- 2023/Day4/part2.ps1
+++ 2023/Day4/part2.ps1
@@ -1,3 +1,45 @@
 Clear-Host
-$data = Get-Content "D:\DevOps\AdventOfCode\2023\Day4\testpart1.txt"
-#$data = Get-Content "D:\DevOps\AdventOfCode\2023\Day4\input.txt"
+#$data = Get-Content "D:\DevOps\AdventOfCode\2023\Day4\testpart1.txt"
+$data = Get-Content "D:\DevOps\AdventOfCode\2023\Day4\input.txt"
+
+
+function Get-Matches{
+  param(
+    $cards,
+    $winners
+  )
+  $count = 0
+  foreach($card in $cards){
+    if($card.value -in $winners.value){
+      $count++
+    }
+  }
+  return $count
+}
+$cardCount = @{}
+for($i = 1; $i -le $data.Count; $i++){
+  if(!$cardCount.ContainsKey($i)){
+    $cardCount.$i = 1
+  }
+  else{
+    $cardCount.$i += 1
+  }
+  $sections = ($data[$i - 1] -split ":")[1] -split "\|"
+  $cards = [REGEX]::Matches($sections[0],"\d+")
+  $winners = [REGEX]::Matches($sections[1],"\d+")  
+  "Card $($i): $($cardCount.$i)"
+  for($x = 0; $x -lt $cardCount.$i; $x++){
+    $win = Get-Matches -cards $cards -winners $winners
+    for($y = $i + 1; $y -lt ($i + $win + 1); $y++){
+      if($y -gt $data.count){break}
+      if(!$cardCount.ContainsKey($y)){
+        $cardCount.$y = 1
+      }
+      else{
+        $cardCount.$y += 1
+      }
+    }
+  }
+  
+}
+$cardCount.Values | Measure-Object -Sum
