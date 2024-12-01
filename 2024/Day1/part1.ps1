bb74e2c1cc96e4e80de3ef6cf0eab6ba3bf0c645
Jeremy Putman <jeremy.putman@ugdsb.on.ca>
2024-12-01 15:37:19 -0500

    Optimized with inspiration from Mamue solution

--- 2024/Day1/part1.ps1
+++ 2024/Day1/part1.ps1
@@ -1,7 +1,30 @@
+using namespace System.Collections.Generic
 Clear-Host
+# Inspired by Mamue.  
+Measure-Command {
+  $list1 = [List[int]]::new()
+  $list2 = [List[int]]::new()
+  switch -Regex -File "D:\DevOps\AdventOfCode\2024\Day1\input.txt"  {
+    '^\s*(?<left>\d+)\s+(?<right>\d+)\s*$' {
+        $list1.Add([int]$matches['left'])
+        $list2.Add([int]$matches['right'])
+    }
+    Default {}
+  } 
+}
+$list1.sort()
+$list2.sort()
+$total = 0
+for($i = 0; $i -lt $list1.count; $i++){
+  $total += [math]::Abs($list2[$i] - $list1[$i])
+}
+$total
+
+# Original
+Measure-Command {
 $data = Get-Content "D:\DevOps\AdventOfCode\2024\Day1\input.txt"
-$list1 = [System.Collections.Generic.List[int]]@()
-$list2 = [System.Collections.Generic.List[int]]@()
+$list1 = [List[int]]@()
+$list2 = [List[int]]@()
 foreach($item in $data){
   $temp = $item -split '\s+'
   $list1.add($temp[0]) | Out-Null
@@ -13,4 +36,7 @@ $total = 0
 for($i = 0; $i -lt $list1.count; $i++){
   $total += [math]::Abs($list2[$i] - $list1[$i])
 }
+}
 $total
+
+#>
--- 2024/Day1/part2.ps1
+++ 2024/Day1/part2.ps1
@@ -1,4 +1,26 @@
+using namespace System.Collections.Generic
 Clear-Host
+# Inspired by Mamue.  
+Measure-Command {
+  $list1 = [List[int]]::new()
+  $list2 = [List[int]]::new()
+  switch -Regex -File "D:\DevOps\AdventOfCode\2024\Day1\input.txt"  {
+    '^\s*(?<left>\d+)\s+(?<right>\d+)\s*$' {
+        $list1.Add([int]$matches['left'])
+        $list2.Add([int]$matches['right'])
+    }
+    Default {}
+  } 
+  $total = 0
+  foreach($item in $list1){
+    if($list2.contains($item)){
+      $total += $item * ($list2 | Where-Object {$_ -eq $item}).count 
+    }
+  }  
+}
+$total
+
+# Original
 Measure-Command {
 $data = Get-Content "D:\DevOps\AdventOfCode\2024\Day1\input.txt"
 $list1 = [System.Collections.Generic.List[int]]@()
@@ -12,5 +34,6 @@ $total = 0
 foreach($item in $list1){
   $total += ($list2 | Where-Object {$_ -eq $item}).count * $item
 }
-$total
 }
+$total
+
